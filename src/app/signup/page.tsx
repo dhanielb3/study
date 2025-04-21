ca1766480691240f76e2e5b784d48be73c92e524
dhanielb3 <dhanielbrandao3@gmail.com>
2025-04-21 12:44:42 -0300

    online

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -45,7 +45,7 @@ export default function Home() {
 			if (fotoFile) {
 				const formData = new FormData();
 				formData.append("UPLOADCARE_STORE", "1");
-				formData.append("UPLOADCARE_PUB_KEY", "SUA_PUBLIC_KEY"); // coloca tua chave aqui
+				formData.append("UPLOADCARE_PUB_KEY", process.env.NEXT_PUBLIC_UPLOADCARE_PUBLIC_KEY || ""); // coloca tua chave aqui
 				formData.append("file", fotoFile);
 
 				try {
